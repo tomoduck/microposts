360e1f029d1aac056c8dcdf1de59464c08a2bbc1
tomosuke <tomo.yoropiko@hotmail.co.jp>
2016-01-08 06:43:09 +0000

    kaminari

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create]
 
+
    def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -12,6 +13,7 @@ class MicropostsController < ApplicationController
     end
    end
   
+
   def destroy
     @micropost = current_user.microposts.find_by(id: params[:id])
     return redirect_to root_url if @micropost.nil?
@@ -20,7 +22,7 @@ class MicropostsController < ApplicationController
     redirect_to request.referrer || root_url
   end
 
-  
+    
   
   private
   def micropost_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
                                         :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
      @users = User.all
   end
   
-  #showing twitter
+  #showing microposts
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(created_at: :desc)
+    @microposts = @user.microposts.order(created_at: :desc).page(params[:page])
   end
   
   def correct_user
@@ -46,7 +46,6 @@ class UsersController < ApplicationController
     @title = "Following"
     @user  = User.find(params[:id])
     @users = @user.following_users(page: params[:page])
-  
   end
 
   def followers
